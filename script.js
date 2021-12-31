32bacf41f4d16be5b1d2bfc0a03df3814e2b9d7e
Noor Ali <nooralib180@gmail.com>
2021-12-31 23:01:19 +0500

    tab component added

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const section1 = document.querySelector('#section-1');
 const navLinks = document.querySelector('.nav_links');
 const nav = document.querySelector('.nav');
 const header = document.querySelector('.header');
+const tabsContainer = document.querySelector('.operations_tab-container');
+const operationTabs = document.querySelectorAll('.operations_tab');
+const tabsContent = document.querySelectorAll('.operations_content')
 
 // Smooth Scrolling...
 scrollToBtn.addEventListener('click', function(e){
@@ -40,14 +43,15 @@ const stickTheNav = function(entries, observer){
     else nav.classList.remove('sticky');
 }
 
-const observer = new IntersectionObserver(stickTheNav, {
+const headerObserver = new IntersectionObserver(stickTheNav, {
     root: null,
     threshold: 0,
     rootMargin: '-85px',
 });
 
-observer.observe(header);
+headerObserver.observe(header);
 
+// observing the navbar.
 const navObserver = new IntersectionObserver(function(entries, observer){
     const [entry] = entries;
     if(!entry.isIntersecting) nav.classList.add('transform');
@@ -57,3 +61,22 @@ const navObserver = new IntersectionObserver(function(entries, observer){
     threshold: 0,
 });
 navObserver.observe(nav);
+
+// Tabbed component implementation...
+tabsContainer.addEventListener('click', function(e){
+    e.preventDefault();
+
+    const clicked = e.target.closest('.operations_tab');
+
+    if(!clicked) return;
+
+    // removing active classes from all tabs.
+    operationTabs.forEach((tab) => tab.classList.remove('operations_tab-active'));
+    // removing active classes from all tab content containers.
+    tabsContent.forEach((content) => content.classList.remove('operations_content-active'));
+
+    // adding active class to the target tab.
+    clicked.classList.add('operations_tab-active');
+    // adding active class to the target content.
+    document.querySelector(`.operations_content-${clicked.dataset.tab}`).classList.add('operations_content-active');
+});
